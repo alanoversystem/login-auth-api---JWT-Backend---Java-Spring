76b2c087983ee54b8c706df8c58e9effc2fb7e9c
alanoversystem <69443191+alanoversystem@users.noreply.github.com>
2025-08-13 21:36:23 -0300

    create path /register in AuthController

---
+++ src/main/java/com/oversystem/login_auth_api/controllers/AuthController.java
@@ -0,0 +1,59 @@
+package com.oversystem.login_auth_api.controllers;
+
+import org.apache.el.stream.Optional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oversystem.login_auth_api.domain.user.User;
+import com.oversystem.login_auth_api.dto.LoginRequestDTO;
+import com.oversystem.login_auth_api.dto.RegisterRequestDTO;
+import com.oversystem.login_auth_api.dto.ResponseDTO;
+import com.oversystem.login_auth_api.infra.security.TokenService;
+import com.oversystem.login_auth_api.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body){
+           User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+           if(passwordEncoder.matches(user.getPassword(), body.password())){
+             String token = this.tokenService.generateToken(user);
+             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+           }
+
+           return ResponseEntity.badRequest().build();
+        }
+
+        @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body){
+           Optional<User> user = this.repository.findByEmail(body.email());
+
+           if(user.isEmpty()){
+           User newUser =  new User();
+           newUser.setName(body.name());
+           newUser.setEmail(body.email());
+           newUser.setPassword(passwordEncoder.encode(body.password()));
+           this.repository.save(newUser);
+
+             String token = this.tokenService.generateToken(user);
+             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+              }
+            
+
+           return ResponseEntity.badRequest().build();
+        }    
+
+}
---
+++ src/main/java/com/oversystem/login_auth_api/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.oversystem.login_auth_api.dto;
+
+public record LoginRequestDTO (String email, String password){
+
+}
